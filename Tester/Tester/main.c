e625a3c8daab453a1a065fe9d094f1a70c40c637
carson-jenkins <cjenkin5@uccs.edu>
2022-08-30 10:41:22 -0600

    Initial commit

---
+++ Tester/Tester/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Tester
+//
+//  Created by Carson Jenkins on 8/30/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, Worl!\n");
+    return 0;
+}
